90dc25248b7d54bea9fb1b8d547d57f3b4ef9a5f
Aero-hacker <jagan.devarapu@gmail.com>
2023-11-01 18:07:55 +0530

    commit

--- list/src/App.jsx
+++ list/src/App.jsx
@@ -19,9 +19,24 @@ function App() {
 
       ]
     })
-
+    setNewitem("")
 
   }
+   function toggleTodo(id,completed){
+    setTodos(currentTodos => {
+      return(currentTodos.map(todo => {
+        if (todo.id ===id ) {
+          return{...todo,coompleted}
+        }
+        return todo
+      }))
+    })
+   }
+   function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+   }
   
   return (
     <>
@@ -40,14 +55,22 @@ function App() {
    </form>
    <h1 className="header"> Todo List</h1>
    <ul className="list">
+    {todos.length === 0 && "No Todos"}
      {todos.map(todo => {
       return(
         <li key={todo.id}>
        <label >
-        <input type="checkbox" checked = {todo.completed}/>
+        <input type="checkbox" 
+        checked = {todo.completed}
+        onChange={e => toggleTodo(todo.id, e.target.checked)}
+        />
          {todo.title}
        </label>
-       <button className="btn btn-danger">Delete</button>
+       <button 
+       className="btn btn-danger"
+       onClick={() => deleteTodo(todo.id)}
+       >
+        Delete</button>
     </li>
       )
      })}
